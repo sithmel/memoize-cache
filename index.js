e2d65bec107473c396f421398c0e21996478703e
Maurizio Lupo <maurizio.lupo@gmail.com>
2017-08-16 22:23:45 +0100

    fixed paths in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var CacheRAM = require('./src/cache-ram');
-var NoCache = require('./src/no-cache');
-var BaseCache = require('./src/base-cache');
+var CacheRAM = require('./cache-ram');
+var NoCache = require('./no-cache');
+var BaseCache = require('./base-cache');
 var keyGetter = require('memoize-cache-utils/key-getter');
 var keysGetter = require('memoize-cache-utils/keys-getter');
 
