68c47590da99c9dd83745d3299cfcd6d8b16d126
sithmel <maurizio.lupo@gmail.com>
2018-11-10 22:34:22 +0000

    removed memoize-cache-utils

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var CacheRAM = require('./cache-ram')
 var NoCache = require('./no-cache')
 var BaseCache = require('./base-cache')
-var keyGetter = require('memoize-cache-utils/key-getter')
-var keysGetter = require('memoize-cache-utils/keys-getter')
 
 module.exports = {
   CacheRAM: CacheRAM,
   NoCache: NoCache,
-  BaseCache: BaseCache,
-  keyGetter: keyGetter,
-  keysGetter: keysGetter
+  BaseCache: BaseCache
 }
